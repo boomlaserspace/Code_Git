76107c21da0166e9e69bf1bd2f25231c26646fbf
David Oexle <davidoexle@gmail.com>
2022-08-22 21:27:28 +0200

    Copied files from Original for Git and GitHub

---
+++ cylindrical_coordinates_v2.py
@@ -0,0 +1,53 @@
+from generate_xdmf_mesh import *
+from os import path
+from dolfin import*
+from dolfin import grad
+from matplotlib import pyplot as plt
+from ufl import as_tensor
+from ufl import indices
+import numpy as np
+
+
+
+def div_cylindrical_vector(vec):  ##### hier wird ein skalar zurückgegeben 
+    
+    r_inv = Expression("1.0/x[0]", degree=2)
+
+    scalar_cylindrical_grad = vec[0].dx(0) + r_inv*vec[0] + vec[1].dx(1)
+
+    return scalar_cylindrical_grad
+
+def grad_cylindircal_vector(vec): ##### hier wird ein Tensor zurückgegeben 
+    r_inv = Expression("1.0/x[0]", degree=2)
+
+    tensor_clyindrical_grad = as_tensor( 
+        [[ vec[0].dx(0), 0, vec[0].dx(1)], 
+         [0,r_inv * vec[1] ,0],
+         [ vec[1].dx(0), 0, vec[1].dx(1)]] 
+        )
+    return tensor_clyindrical_grad
+
+def outer_expend(vec1,vec2):  ### 2D vektoren werden erweiter sodass es passt am Ende
+
+    expanded_tensor = as_tensor( 
+        [[ vec1[0]*vec2[0], 0, vec1[0]*vec2[1]], 
+         [ 0,0,0],
+         [ vec1[1]*vec2[0], 0, vec1[1]*vec2[1]]] 
+        )
+    return expanded_tensor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
